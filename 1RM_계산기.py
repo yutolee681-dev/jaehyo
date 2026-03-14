801c9073954d1c65253a55a8ab0711643dd2d9d6
Hyo <yutolee681@gmail.com>
2026-03-14 10:28:55 +0900

    Update 1RM_계산기.py

--- 1RM_계산기.py
+++ 1RM_계산기.py
@@ -79,7 +79,6 @@ def get_comments():
 
 
 df = get_full_data()
-st.write(df)
 comments_df = get_comments()
 
 if 'is_auth' not in st.session_state:
@@ -314,6 +313,7 @@ with st.expander("🛠️ Admin"):
     admin_pw = st.text_input("Key", type="password")
     if admin_pw == "5207":
         st.dataframe(df)
+
 
 
 
